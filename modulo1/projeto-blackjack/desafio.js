85b8bdcfe8ec945d9047f5188425913be1a98405
Daniel Matos <danielsegatto5@gmail.com>
2022-03-11 21:45:01 -0300

    adicionei o desafio

---
+++ modulo1/projeto-blackjack/desafio.js
@@ -0,0 +1,59 @@
+console.log("Boas vindas ao jogo de Blackjack!")
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+  const usuario = {
+    cartas: [],
+    pontos: 0,
+    addCarta: function (novaCarta) {
+      this.cartas = [...this.cartas, novaCarta.texto]
+      this.pontos += novaCarta.valor
+    },
+    zerarCartas: function () {
+      this.cartas = []
+      this.pontos = 0
+    }
+  }
+  const computador = { ...usuario }
+
+  let distribuindoCartas = true
+  while (distribuindoCartas) {
+    for (let i = 0; i < 2; i++) {
+      usuario.addCarta(comprarCarta())
+      computador.addCarta(comprarCarta())
+    }
+
+    if (usuario.pontos === 22 || computador.pontos === 22) {
+      usuario.zerarCartas()
+      computador.zerarCartas()
+    }
+    else distribuindoCartas = false
+  }
+
+  let comprarMaisCarta = true
+  while (comprarMaisCarta) {
+    comprarMaisCarta = confirm(
+      `Suas cartas são ${usuario.cartas.map(carta => "[" + carta + "]")}. A carta revelada do computador é ${computador.cartas[0]}.` +
+      "\n" +
+      "Deseja comprar mais uma carta?"
+    )
+
+    if(comprarMaisCarta) usuario.addCarta(comprarCarta())
+    if(usuario.pontos >= 21) comprarMaisCarta = false
+  }
+
+  let mensagem = (usuario, computador) =>{
+    return `Suas cartas são ${usuario.cartas.map(carta => "[" + carta + "]")} . \nSua pontuação é ${usuario.pontos}.`+
+    `\n\nAs cartas do computador são ${computador.cartas.map(carta => "[" + carta + "]")} . \nA pontuação do computador é ${computador.pontos}.`
+  } 
+
+  if(usuario.pontos > 21) alert(mensagem(usuario, computador) +  `\n\nO computador ganhou!`)
+
+  if(usuario.pontos <= 21) {
+    while(computador.pontos < usuario.pontos) {
+      computador.addCarta(comprarCarta())
+    }
+    if(computador.pontos === usuario.pontos) alert(mensagem(usuario, computador) + `\n\nEmpate!`)
+    else if(computador.pontos > 21 || usuario.pontos > computador.pontos) alert(mensagem(usuario, computador) + `\n\nO usuário ganhou!`)
+    else if (computador.pontos > usuario.pontos) alert(mensagem(usuario, computador) + `\n\nO computador ganhou!`)
+  }
+}
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,11 +1,10 @@
 console.log("Boas vindas ao jogo de Blackjack!")
 
-// if(confirm("Quer iniciar uma nova rodada?")) {
-if(true){
+if(confirm("Quer iniciar uma nova rodada?")) {
   const usuario = {
     cartas: [],
     pontos: 0,
-    addCarta: function({novaCarta}) {
+    addCarta: function(novaCarta) {
       this.cartas = [...this.cartas, novaCarta.texto] // usando o metodo push(), todos os obj copiados usando o spread também tem a propriedade alterada
       this.pontos += novaCarta.valor
     }
@@ -17,8 +16,8 @@ if(true){
     computador.addCarta(comprarCarta())
   }
 
-  console.log(`Usuário: ${usuario.cartas.map(carta => "["+carta+"]")} - ${usuario.pontos}`)
-  console.log(`Computador: ${computador.cartas.map(carta => "["+carta+"]")} - ${computador.pontos}`)
+  console.log(`Usuário: ${usuario.cartas.map(carta => "["+carta+"]")} -> ${usuario.pontos}`)
+  console.log(`Computador: ${computador.cartas.map(carta => "["+carta+"]")} -> ${computador.pontos}`)
 
   if(usuario.pontos > computador.pontos) console.log("O usuário ganhou!")
   else if(usuario.pontos < computador.pontos) console.log("O computador ganhou!")
