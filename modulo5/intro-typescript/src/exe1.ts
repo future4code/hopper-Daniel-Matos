2258c13a8504ecae02e37039b13d5a21122e3b73
Daniel Matos <danielsegatto5@gmail.com>
2022-09-03 19:50:55 -0300

    realizei 5 exercicios

---
+++ modulo5/intro-typescript/src/exe1.ts
@@ -0,0 +1,9 @@
+export default function checaTriangulo(a: number, b: number, c:number): string {
+  if (a !== b && b !== c) {
+    return "Escaleno";
+  } else if (a === b && b === c) {
+    return "Equilátero";
+  } else {
+    return "Isósceles";
+  }
+}
---
+++ modulo5/intro-typescript/src/exe2.ts
@@ -0,0 +1,3 @@
+export default function imprimeTresCoresFavoritas(cor1: string, cor2: string, cor3: string): string[] {
+  return ([cor1, cor2, cor3])
+}
