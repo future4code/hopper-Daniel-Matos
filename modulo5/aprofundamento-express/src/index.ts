a775d3bcbb51c13ee5d3047263299301fec0a550
Daniel Matos <danielsegatto5@gmail.com>
2022-09-15 18:39:12 -0300

    ate exe 5

---
+++ modulo5/aprofundamento-express/src/index.ts
@@ -0,0 +1,57 @@
+import express, { Request, response, Response } from 'express'
+import cors from 'cors'
+import { v4 as generateId } from 'uuid';
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+// EXE 2
+type Todo = {
+    userId: string,
+    id: string,
+    title: string,
+    completed: boolean,
+}
+// EXE 3
+const todos: Todo[] = [
+    { userId: "1", id: "1", title: "todo 1", completed: true },
+    { userId: "1", id: "2", title: "todo 2", completed: false },
+    { userId: "2", id: "3", title: "todo 3", completed: true },
+    { userId: "3", id: "4", title: "todo 4", completed: false }
+]
+
+// EXE 1
+app.get("/ping", (req: Request, res: Response) => {
+    res.status(200).send("Pong")
+})
+
+// EXE 5
+app.post("/todos", (req: Request, res: Response) => {
+    const newTodo = req.body as Todo
+    todos.push(newTodo)
+
+    res.status(200).send(todos)
+})
+
+// EXE 4
+app.get("/todos", (req: Request, res: Response) => {
+    const completed = (req.query.completed === "true")
+    
+    const filteredTodos: Todo[] = todos.filter((todo) => {
+        return (completed && todo.completed) || (!completed && !todo.completed)
+    })
+
+    res.status(200).send(filteredTodos)
+})
+
+
+app.delete("", (request: Request, response: Response) => { })
+
+
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+})
